e2e520adc869b23161072a631f49d0f505d16ee0
jufe <jufesena@gmail.com>
2026-05-15 08:13:56 -0500

    fix: reset FreezableBinLayout and force full relayout to shrink calendar popup

--- configs/gnome/calendar-tweaks/extension.js
+++ configs/gnome/calendar-tweaks/extension.js
@@ -8,41 +8,42 @@ export default class CalendarTweaks extends Extension {
         this._box = this._messageList.get_parent();
         if (!this._box) return;
 
-        const children = this._box.get_children();
-        const msgIdx = children.indexOf(this._messageList);
-        this._separator = msgIdx > 0 ? children[msgIdx - 1] : null;
-        if (this._separator?.get_n_children?.() > 0)
-            this._separator = null;
-
-        if (this._separator)
-            this._box.remove_child(this._separator);
         this._box.remove_child(this._messageList);
 
-        this._styledActors = [];
+        this._bin = this._box.get_parent();
+        if (this._bin?.layout_manager?.frozen !== undefined)
+            this._bin.layout_manager.frozen = false;
+
+        this._forceRelayout();
+
+        this._openId = dateMenu.menu.connect('open-state-changed', (_menu, isOpen) => {
+            if (!isOpen) return;
+            if (this._bin?.layout_manager?.frozen !== undefined)
+                this._bin.layout_manager.frozen = false;
+            this._forceRelayout();
+        });
+        this._dateMenu = dateMenu;
+    }
+
+    _forceRelayout() {
         let actor = this._box;
         while (actor) {
-            if (actor.add_style_class_name) {
-                actor.add_style_class_name('ct-compact');
-                this._styledActors.push(actor);
-            }
-            if (actor.has_style_class_name?.('popup-menu-boxpointer'))
-                break;
+            if (actor.queue_relayout)
+                actor.queue_relayout();
             actor = actor.get_parent();
         }
     }
 
     disable() {
-        for (const a of this._styledActors || [])
-            a.remove_style_class_name('ct-compact');
-        if (this._box) {
-            if (this._separator)
-                this._box.add_child(this._separator);
-            if (this._messageList)
-                this._box.add_child(this._messageList);
+        if (this._openId) {
+            this._dateMenu.menu.disconnect(this._openId);
+            this._openId = null;
         }
+        if (this._box && this._messageList)
+            this._box.insert_child_at_index(this._messageList, 0);
         this._messageList = null;
-        this._separator = null;
         this._box = null;
-        this._styledActors = null;
+        this._bin = null;
+        this._dateMenu = null;
     }
 }
